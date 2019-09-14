5c24f63cc87796cfda11eca97c5e48da1576c39a
raghuvanshraj <raghuvansh1997@gmail.com>
2019-09-14 15:10:17 +0530

    all files added

---
+++ get-details.js
@@ -0,0 +1,19 @@
+"use strict";
+
+var request = new XMLHttpRequest()
+
+request.open('GET', 'https://codeforces.com/api/user.info?handles=raghuvanshraj', true)
+
+request.onload = function() {
+    var result = JSON.parse(this.response)
+    console.log(request.status)
+    if (request.status >= 200 && request.status < 400) {
+        console.log(result.result[0].rank)
+        console.log(result.result[0].rating)
+        document.getElementById("codeforces-details").innerHTML = "Rank: " + result.result[0].rank.charAt(0).toUpperCase() + result.result[0].rank.substring(1) + "<br>Rating: " + result.result[0].rating
+    } else {
+        console.log('error')
+    }
+}
+
+request.send()
